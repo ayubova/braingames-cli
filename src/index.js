b2ef01a0f6d46f6b73b0169ad66a20fe294058e7
ayubova <ayubova.yuliya@gmail.com>
2017-10-11 13:39:13 +0300

    add fourth step

--- src/index.js
+++ src/index.js
@@ -12,19 +12,16 @@ export const brainEven = () => {
   console.log('Answer "yes" if number even otherwise answer "no".');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  for (let i = 1; i <= 3; i + 1) {
-    const number = Math.floor(Math.random() * 1000);
-    const userAnswer = readlineSync.question(`Question: ${number}\nYour answer: `);
-    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  for (let i = 1; i <= 3; i += 1) {
+    const question = Math.floor(Math.random() * 1000);
+    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+    const isEven = num => num % 2 === 0;
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
     if (correctAnswer !== userAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${userName}!`);
       return;
     }
-    if (i === 3 && correctAnswer === userAnswer) {
-      console.log(`Congratulations, ${userName}!`);
-    } else {
-      console.log('Correct!');
-    }
+    console.log('Correct!');
   }
+  console.log(`Congratulations, ${userName}!`);
 };
-
