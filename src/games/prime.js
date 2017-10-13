867e57f31ee6929527b81f9845117a437caa2762
ayubova <ayubova.yuliya@gmail.com>
2017-10-13 20:05:53 +0300

    add nine step

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,12 +4,15 @@ import getRandom from '../random';
 const rule = 'Answer "yes" if number is prime otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
-  return num !== 1;
+  return true;
 };
 
 export default () => {
