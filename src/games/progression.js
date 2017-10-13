3d3fe40794b8d5a6eaaa802f43f6f00f4738ee7b
ayubova <ayubova.yuliya@gmail.com>
2017-10-13 19:30:23 +0300

    add nine step

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,15 +3,16 @@ import getRandom from '../random';
 
 const rule = 'What number is missing in this progression?';
 
-const generateProgression = (first, d) => {
+const generateProgression = (first, difference) => {
   let result = [first];
-  for (let i = 0; i < 9; i += 1) {
-    result = result.concat(result[result.length - 1] + d);
+  const maxLength = 10;
+  for (let i = 1; i < maxLength; i += 1) {
+    result = result.concat(result[result.length - 1] + difference);
   }
   const missingIndex = getRandom(9);
   const missingElement = result[missingIndex];
   result[missingIndex] = '..';
-  return [missingElement, result];
+  return [missingElement, result.join(' ')];
 };
 
 export default () => {
